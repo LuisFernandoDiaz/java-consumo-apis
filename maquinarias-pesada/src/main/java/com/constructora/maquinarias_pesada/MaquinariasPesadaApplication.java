64067fdb80709d57170a129ee2083d075c8455a4
fdiaz <luisfernandodiaz.c2001@gmail.com>
2025-10-17 23:41:02 -0500

    primera publicacion

---
+++ maquinarias-pesada/src/main/java/com/constructora/maquinarias_pesada/MaquinariasPesadaApplication.java
@@ -0,0 +1,13 @@
+package com.constructora.maquinarias_pesada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquinariasPesadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaquinariasPesadaApplication.class, args);
+	}
+
+}
---
+++ maquinarias-pesada/src/test/java/com/constructora/maquinarias_pesada/MaquinariasPesadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constructora.maquinarias_pesada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaquinariasPesadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
